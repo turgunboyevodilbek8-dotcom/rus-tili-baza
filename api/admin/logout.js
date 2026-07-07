08f5679c2dda5ef4bd6c41d894c8edf2eb3f7e67
turgunboyevodilbek8-dotcom <turgunboyevodilbek8@gmail.com>
2026-07-07 10:54:20 +0500

    rus tili

--- api/admin/logout.js
+++
@@ -1,4 +0,0 @@
-module.exports = async function handler(req, res) {
-  res.setHeader('Set-Cookie', 'baza_admin=; Path=/; HttpOnly; Secure; SameSite=Strict; Max-Age=0');
-  return res.status(200).json({ ok: true });
-};
